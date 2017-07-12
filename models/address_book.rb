ee8447343bdc9e83fbfbe6329b492c77b2c38618
Tim Clark <tim.clark@outlook.com>
2017-07-12 18:35:46 +0000

    assignment4

--- models/address_book.rb
+++ models/address_book.rb
@@ -7,13 +7,24 @@
       @entries = []
      end
 
- 
+     def remove_entry(name, phone, email)
+        delete_entry = nil
+        
+        @entries.each do |entry|
+            if name == entry.name && phone == entry.phone_number && email == entry.email
+                delete_entry = entry
+            end         
+        end
+        
+            @entries.delete(delete_entry)
+        end
+    
      
-   def add_entry(name, phone_number, email)
+     def add_entry(name, phone_number, email)
      # #9
-     index = 0
-     entries.each do |entry|
-     # #10
+        index = 0
+        entries.each do |entry|
+         # #10
        if name < entry.name
          break
        end
--- spec/address_book_spec.rb
+++ spec/address_book_spec.rb
@@ -16,24 +16,41 @@ require_relative '../models/address_book'
        book = AddressBook.new
        expect(book.entries.size).to eq(0)
      end
-   end
+    end
    
+      context "#remove_entry" do
+        it "removes an entry using the name, phone_number, and email address" do
+            book = AddressBook.new
+            book.add_entry('Tim Clark', '010.013.0000', 'tc@tc.com')
+            
+            name = "Ada Lovelace"
+            phone_number = "010.012.1815"
+            email = "augusta.king@lovelace.com"
+            book.add_entry(name, phone_number, email)
+            
+            expect(book.entries.size).to eq 2
+            book.remove_entry(name, phone_number, email)
+            expect(book.entries.size).to eq 1
+            expect(book.entries.first.name).to eq("Tim Clark")
+     end
+    end
+    
       describe "#add_entry" do
-     it "adds only one entry to the address book" do
-       book = AddressBook.new
-       book.add_entry('Ada Lovelace', '010.012.1815', 'augusta.king@lovelace.com')
+        it "adds only one entry to the address book" do
+            book = AddressBook.new
+            book.add_entry('Ada Lovelace', '010.012.1815', 'augusta.king@lovelace.com')
  
        expect(book.entries.size).to eq(1)
      end
  
-     it "adds the correct information to entries" do
-       book = AddressBook.new
-       book.add_entry('Ada Lovelace', '010.012.1815', 'augusta.king@lovelace.com')
-       new_entry = book.entries[0]
+        it "adds the correct information to entries" do
+            book = AddressBook.new
+            book.add_entry('Ada Lovelace', '010.012.1815', 'augusta.king@lovelace.com')
+            new_entry = book.entries[0]
  
        expect(new_entry.name).to eq('Ada Lovelace')
        expect(new_entry.phone_number).to eq('010.012.1815')
        expect(new_entry.email).to eq('augusta.king@lovelace.com')
      end
+    end
    end
- end
