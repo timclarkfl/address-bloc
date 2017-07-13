8d52241a2856545c52fac465914d39b5d5ec5915
Tim Clark <tim.clark@outlook.com>
2017-07-13 01:35:13 +0000

    chkpoint6

--- models/address_book.rb
+++ models/address_book.rb
@@ -1,25 +1,33 @@
  require_relative 'entry'
- 
- class AddressBook
-     attr_reader :entries
-     
-     def initialize
-      @entries = []
-     end
+ require "csv"
 
- 
-     
-   def add_entry(name, phone_number, email)
-     # #9
-     index = 0
-     entries.each do |entry|
-     # #10
-       if name < entry.name
-         break
-       end
-       index+= 1
+ class AddressBook
+   attr_reader :entries
+
+  def initialize
+    @entries = []
+  end
+
+  def add_entry(name, phone_number, email)
+    index = 0
+    entries.each do |entry|
+      if name < entry.name
+        break
+      end
+      index += 1
      end
-     # #11
      entries.insert(index, Entry.new(name, phone_number, email))
    end
+ 
+   def import_from_csv(file_name)
+     csv_text = File.read(file_name)
+     csv = CSV.parse(csv_text, headers: true, skip_blanks: true)
+   # #8
+     csv.each do |row|
+       row_hash = row.to_hash
+       add_entry(row_hash["name"], row_hash["phone_number"], row_hash["email"])
+     end
+   end
  end
+ 
+ 
